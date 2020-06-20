42bfc8d153b628a2ba74eb7916156ce6551ebfe7
calvinreu <calvinreu@localhost.localdomain>
2020-06-20 12:54:50 +0200

    changed to multiple data chuncks instead of one

--- matrix/matrix.h
+++ matrix/matrix.h
@@ -1,22 +1,18 @@
 #pragma once
 #include <extended/vector>
-#include <memory>
+#include <extended/matrix>
 #include <error.h>
 #include <initializer_list>
-#include <new>
 #include <iostream>
-#include <string.h>
 
 
 namespace extended{
 
 	template<typename T>
-	class matrix{
+	class multi_vector_matrix{
 	private:
 		//data container and data management
-		extended::vector<T> data;
-		size_t rowSize;
-		inline const T& operator[](const size_t &index) const { return data[index]; }
+		extended::vector<extended::vector<<T>> data;
 
 		template<typename t = T>
 		inline typename std::enable_if<!std::is_trivially_copy_constructible<t>::value>::type
@@ -31,48 +27,43 @@ namespace extended{
  
 	public:
 
-		matrix(){}
-		matrix(const size_t &rowCount, const size_t &rowSize) : data(rowCount*rowSize), rowSize(rowSize){}//constructor only allocates memory to initialize fill operator = also works but will destroy the matrix first
-		matrix(const size_t &size) : data(size){}
-		matrix(const std::initializer_list<std::initializer_list<T>> initData);
-		~matrix(){}//do nothing data already deallocates memory
-		matrix(      matrix<T> &&other) : data(std::move(other.data)), rowSize(other.rowSize){other.rowSize = 0; }
-		matrix(const matrix<T>  &other) : data(other.data), rowSize(other.rowSize){}
+		multi_vector_matrix(){}
+		multi_vector_matrix(const size_t &rowCount, const size_t &rowSize);//constructor only allocates memory to initialize fill operator = also works but will destroy the matrix first
+		multi_vector_matrix(const std::initializer_list<std::initializer_list<T>> initData);
+		~multi_vector_matrix(){}//do nothing data already deallocates memory
+		multi_vector_matrix(      multi_vector_matrix<T> &&other) : data(std::move(other.data)){}
+		multi_vector_matrix(const multi_vector_matrix<T>  &other) : data(other.data){}
 
-		T& operator()(const size_t &row, const size_t &index){ return data[(row*rowSize)+index]; }
-		const T& operator()(const size_t &row, const size_t &index) const { return data[(row*rowSize)+index]; }
+        void operator=(const multi_vector_matrix<T>  &other){ data = other.data; }
+		void operator=(	  multi_vector_matrix<T> &&other){ data = std::move(other.data);}
+        void operator=(const std::initializer_list<std::initializer_list<T>> initData){ this->data = std::move(extended::multi_vector_matrix(initData)); }
 
-		matrix<T> operator+(const matrix<T> &other) const;
-		matrix<T> operator-(const matrix<T> &other) const;
-		matrix<T> operator*(const matrix<T> &other) const;
-		matrix<T> operator*(const T        &scalar) const;//scalar multiplication
-		matrix<T> operator/(const matrix<T> &other) const;
-		//matrix<T> operator%(const matrix<T> &other) const; not sure if usefull or what it shoulkd do
+		T& operator()(const size_t &row, const size_t &index){ return data[row][index]; }
+		const T& operator()(const size_t &row, const size_t &index) const { return data[row][index]; }
 
-		void operator-=(const matrix<T>  &other){ *this = (*this) - other; }
-		void operator+=(const matrix<T>  &other){ *this = (*this) + other; }
-		void operator*=(const matrix<T>  &other){ *this = (*this) * other; }
-		void operator*=(const T          &other){ *this = (*this) * other; }
-		//void operator/=(const matrix<T>  &other){ *this = (*this) / other; } //not sure if usefull or what it should do
+		multi_vector_matrix<T> operator+(const multi_vector_matrix<T> &other) const;
+		multi_vector_matrix<T> operator-(const multi_vector_matrix<T> &other) const;
+		multi_vector_matrix<T> operator*(const multi_vector_matrix<T> &other) const;
+		multi_vector_matrix<T> operator*(const T &scalar) const;//scalar multiplication
+		//multi_vector_matrix<T> operator/(const multi_vector_matrix<T> &other) const;
+		//multi_vector_matrix<T> operator%(const multi_vector_matrix<T> &other) const; not sure if usefull or what it shoulkd do
 
-		void operator=(const matrix<T>  &other){ data = other.data;
-												  rowSize = other.rowSize; }
-		void operator=(	  matrix<T> &&other){ data = std::move(other.data);
-												  rowSize = std::move(other.rowSize); }
-		void operator=(const std::initializer_list<std::initializer_list<T>> initData);
+		void operator-=(const multi_vector_matrix<T> &other){ *this = (*this) - other; }
+		void operator+=(const multi_vector_matrix<T> &other){ *this = (*this) + other; }
+		void operator*=(const multi_vector_matrix<T> &other){ *this = (*this) * other; }
+		void operator*=(const T &other){ *this = (*this) * other; }
+		//void operator/=(const multi_vector_matrix<T>  &other){ *this = (*this) / other; } //not sure if usefull or what it should do
 
-		const size_t& row_size () const { return rowSize; }
-		const size_t row_count() const { return data.max_size()/row_size(); }
+		const size_t& row_size () const { return data.first.size(); }
+		const size_t row_count() const { return data.size(); }
 
 		template<typename to>
-		matrix<to> cast() const; //TODO src code for this block
-		void resize(const size_t &rowCount, const size_t &rowSize);// deletes all data from the matrix
-			  T* dataPtr()       {return data.get_ValPtr(); }
-		const T* dataPtr() const {return data.get_ValPtr(); }
-		inline const size_t size()     const { return data.size()    ; }
-		inline const size_t max_size() const { return data.max_size(); }
+		multi_vector_matrix<to> cast() const;
+		void recreate(const size_t &rowCount, const size_t &rowSize);// deletes all data from the matrix
+			  extended::vector<T> get_data()       {return data; }
+		const extended::vector<T> get_data() const {return data; }
 		template<typename... args>
-		inline void fill(args &...arguments){ data.assign(this->max_size()-size(), arguments...); }//if it isnt trivially_default_constructible arguments arent used
+		inline void fill(args &...arguments);//if it isnt trivially_default_constructible arguments arent used
 
 		//iterator functions
 		inline typename extended::vector<T>::normal_iterator begin (){ return data.begin (); }
