0e4799fd0c449051dd69dfacad4795185bad0363
calvinreu <calvinreu@localhost.localdomain>
2020-06-21 12:43:48 +0200

    updated test

--- matrix/test.cpp
+++ matrix/test.cpp
@@ -2,8 +2,7 @@
 #include <vector>
 #include <random>
 
-#include "lib_v1/matrix.h"
-#include "lib_v2/matrix"
+#include "matrix"
 
 int main(int argc, char const *argv[]) {
     
@@ -15,18 +14,28 @@ int main(int argc, char const *argv[]) {
         {0.9, 1.1, 1.2},
         {1.3, 1.4, 0.8}
     };
+
+    std::cout << "created matrix a \n";
     extended::matrix<double> b;
+
+    std::cout << "created matrix b \n";
     b = {
         {1, 0, 0},
         {0, 1, 0},
         {0, 0, 1}
     };
+
+    std::cout << "init matrix b \n";
     auto c = a*b;
 
     std::cout << c.row_size() << std::endl;
 
-    for (auto i = c.begin(); i < c.end(); i++)
-        std::cout << *i << std::endl;
+    for (auto iR = c.begin(); iR < c.end(); iR++)
+    {
+        for (auto i = iR->begin(); i < iR->end(); i++)
+            std::cout << *i << std::endl;
+        std::cout << "\n";
+    }
     
     return 0;
 }
