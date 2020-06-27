5d2cd4c7aaa2db91219923a4f4c2b10b669199d7
calvinreu <calvinreu@localhost.localdomain>
2020-06-27 12:00:44 +0200

    changed namespace

--- random/random.hpp
+++ random/random.hpp
@@ -2,16 +2,20 @@
 #include <time.h>
 #include <random>
 
-inline int random_int()
-{
-    srand(time(NULL));
-    return rand();
-}
+namespace extended{
 
-inline double random_double(double fMin, double fMax)
-{
-    srand(time(NULL));
+    inline int random_int()
+    {
+        srand(time(NULL));
+        return rand();
+    }
+    
+    inline double random_double(double fMin, double fMax)
+    {
+        srand(time(NULL));
+    
+        double f = (double)rand() / RAND_MAX;
+        return fMin + f * (fMax - fMin);
+    }
 
-    double f = (double)rand() / RAND_MAX;
-    return fMin + f * (fMax - fMin);
 }
